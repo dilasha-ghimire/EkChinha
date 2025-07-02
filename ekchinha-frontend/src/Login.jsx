bd71dddf15a4de42b3cefbdd767d91094564291f
Dilasha Ghimire <dilashhaa@gmail.com>
2025-07-02 23:07:41 +0545

    update: login ui

--- ekchinha-frontend/src/Login.jsx
+++ ekchinha-frontend/src/Login.jsx
@@ -1,8 +1,50 @@
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
 function Login() {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>Login Page</h1>
-      <p>Put your login form here.</p>
+    <div className="login-page">
+      <header className="header">
+        <div className="logo-title" onClick={() => navigate("/")}>
+          <img src="/logo.webp" alt="Logo" className="logo" />
+          <span className="brand-name">EkChinha</span>
+        </div>
+      </header>
+      <img src="/namaste.png" alt="Welcome" className="welcome-image" />
+      <div className="auth-tabs">
+        <span className="auth-link active">Login</span>
+        <span className="auth-link" onClick={() => navigate("/register")}>
+          Register
+        </span>
+      </div>
+      <div className="login-box">
+        <div className="input-group">
+          <img src="/email.png" alt="Email Icon" className="input-icon" />
+          <div className="floating-label-input">
+            <input type="email" id="email" required placeholder=" " />
+            <label htmlFor="email">Email</label>
+          </div>
+        </div>
+        <div className="input-group">
+          <img
+            src="/lock.png"
+            alt="Password Icon"
+            className="input-icon-lock"
+          />
+          <div className="floating-label-input">
+            <input type="password" id="password" required placeholder=" " />
+            <label htmlFor="password">Password</label>
+          </div>
+        </div>
+        <button className="login-btn">LOGIN</button>
+      </div>
+      <footer className="footer">
+        <div className="footer-copy-wrapper">
+          <p className="footer-copy">Copyright © 2025, EkChinha</p>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ ekchinha-frontend/src/UserRegister.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>Register Page</h1>
+      <p>Put your user register form here.</p>
+    </div>
+  );
+}
+
+export default Login;
